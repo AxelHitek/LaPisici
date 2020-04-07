3e587323704d4eb47df55e9bad015a1e20eff4cc
AlexandruHalalisan <halalisanalexadru@yahoo.com>
2020-04-07 21:40:24 +0300

    base project structure

---
+++ src/main/java/com/lapisici/lapisici/LapisiciApplication.java
@@ -0,0 +1,13 @@
+package com.lapisici.lapisici;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapisiciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapisiciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lapisici/lapisici/LapisiciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lapisici.lapisici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapisiciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
