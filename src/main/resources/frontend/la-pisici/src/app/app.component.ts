d4c100e4291f768c30a1fa5886527427844caefc
AlexandruHalalisan <halalisanalexadru@yahoo.com>
2020-04-16 11:54:56 +0300

    Add placeholder call
    from be

--- src/main/resources/frontend/la-pisici/src/app/app.component.ts
+++ src/main/resources/frontend/la-pisici/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'la-pisici';
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.getCats().subscribe(cat => {
+      console.log(cat);
+      console.log('that was it');
+    });
+  }
+
+  getCats(): Observable<Object> {
+    return this.http.get<Object>('/cat/get-cat', { params: { id: '1' } });
+  }
+
 }
